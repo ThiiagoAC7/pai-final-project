3e9523cf539fc5ef63dd6b988973a0a4c51b9b1c
ThiiagoAC7 <thiiagooac7@gmail.com>
2023-03-06 16:55:31 -0300

    feat: render images as button is pressed

--- main.py
+++ main.py
@@ -7,20 +7,11 @@ import glob
 path = glob.glob('./mamografias/DleftCC/*.png')
 # path = glob.glob('./mamografias/**/*.png')
 
-imgs = []
-for i in path:
-    # print(i)
-    img = Image.open(i)
-    img = img.resize((256, 256))
-    # img = np.asarray(img)
-    imgs.append(img)
-
 data_index = 0
-data_len = len(imgs)
-
-# create figure
-curr_img = plt.imshow(np.asarray(imgs[data_index]), cmap='Greys_r')
-
+data_len = len(path)
+img = Image.open(path[data_index])
+img = img.resize((256, 256))
+curr_img = plt.imshow(np.asarray(img), cmap='Greys_r')
 
 # create Button
 nextax = plt.axes([0.7, 0.025, 0.1, 0.04])  # position of button
@@ -31,7 +22,9 @@ button_prev = Button(prevax, 'prev', hovercolor='0.975')
 
 def update(val):
     global curr_img
-    curr_img.set(data=imgs[val])
+    img = Image.open(path[val])
+    img = img.resize((256, 256))
+    curr_img.set(data=img)
     plt.draw()
 
 
