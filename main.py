7fc3b0205d9f0d8f99e9e4570063110697fb9403
Matheus <matheusmrf@gmail.com>
2023-03-14 20:05:57 -0300

    Funcao de alterar contraste da imagem **
    Precisa de ajustes

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ path = glob.glob('./mamografias/**/*.png')
 data_index = 0
 data_len = len(path)
 img = Image.open(path[data_index])
-img = img.resize((256, 256))
+# img = img.resize((256, 256))
 curr_img = plt.imshow(np.asarray(img), cmap='Greys_r')
 c0 = 3
 axcolor = 'lightgoldenrodyellow'
@@ -23,7 +23,7 @@ resetax = plt.axes([0.8, 0.020, 0.1, 0.04])
 button_reset = Button(resetax, 'Reset', color=axcolor, hovercolor='0.975')
 
 # create slider
-sliderax = plt.axes([0.15, -0.018, 0.65, 0.03], facecolor=axcolor) #position of slider
+sliderax = plt.axes([0.15, 0.9, 0.65, 0.03], facecolor=axcolor)  # position of slider
 scontrast = Slider(sliderax, 'Contrast', 2.1, 30.0, valinit=c0)
 
 
@@ -37,31 +37,41 @@ scontrast = Slider(sliderax, 'Contrast', 2.1, 30.0, valinit=c0)
 def update(val):
     global curr_img
     contrast = scontrast.val
-    print(path[val])
-    img = Image.open(path[val])
-    img = img.resize((256, 256))
-    curr_img.set(data=img)
+    img = Image.open(path[data_index])
+    img = np.asarray(img).astype(float)
+    img = np.interp(img, (np.min(img), np.max(img)), (0, 1))
+    img = np.clip(((img - 0.5) * contrast + 0.5), 0, 1)
+    curr_img.set_data(img)
     plt.draw()
-    scontrast.on_changed(update)
+
 
 def next(event):
     global data_index
     global data_len
     data_index = (data_index + 1) % data_len
-    update(data_index)
+    img = Image.open(path[data_index])
+    curr_img.set_data(np.asarray(img))
+    plt.draw()
+    scontrast.reset()
+
 
 def prev(event):
     global data_index
     global data_len
     data_index = (data_index - 1) % data_len
-    update(data_index)
+    img = Image.open(path[data_index])
+    curr_img.set_data(np.asarray(img))
+    plt.draw()
+    scontrast.reset()
+
 
 def reset(event):
     scontrast.reset()
+
+
 button_reset.on_clicked(reset)
-
-
 
 button_prev.on_clicked(prev)
 button_next.on_clicked(next)
+scontrast.on_changed(update)
 plt.show()
